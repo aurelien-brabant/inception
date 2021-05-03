f89dca87ee2d87529cad76b8bf1587b4bc62a8c5
abrabant <abrabant@student.42.fr>
2021-05-03 16:43:36 +0200

    Add redis ping check before monitoring

--- srcs/redis/scripts/init.sh
+++ srcs/redis/scripts/init.sh
@@ -1,10 +1,21 @@
 #! /bin/sh
 
 if redis-server /etc/myredis.conf --daemonize yes; then
-	printf "[\033[1;32mOK\033[0m] redis-server started successfully!\n"
+	printf "[\033[1;32mOK\033[0m] redis-server daemon started successfully!\n"
 else
 	printf "[\033[1;31mKO\033[0m] redis-server could not be started!\n"
 fi
+
+# Wait for redis to answer to the ping before trying to monitor it
+i=1;
+while [ "$(redis-cli ping 2> /dev/null)" != "PONG" ]; do
+	if [ $i -ge 60 ]; then
+		printf "redis-server took too much time to start properly!\n"
+		exit 1
+	fi
+	printf "Waiting for redis-server to respond to ping... ($i/60 sec)\n"
+	i=$(($i+1))
+done
 
 printf "redis monitoring started...\n"
 redis-cli monitor
